9a74264f91ca0dbfe323bffceb8440cfd406c762
Li0 <manojmarmat762000@gmail.com>
2021-12-12 09:21:59 +0530

    refactor: adding history table

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Form } from "./form";
 export { default as Home } from "./home";
 export { default as Table } from "./table";
 export { default as Modal } from "./modal";
+export { default as Htable } from "./historyTable";
